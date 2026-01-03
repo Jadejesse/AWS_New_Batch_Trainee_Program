bbbdb872763546ad17cdf70992761782e74f6a50
Jadejesse <chosen1edwin@gmail.com>
2026-01-03 13:32:11 +1100

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/YOUR_REPO_NAME/',
+  base: '/AWS_New_Batch_Trainee_Program/',
   plugins: [react()],
 })
