0df4f77c2dd8c8bd7cc93eb82dc5f4ec29ca773a
Jadejesse <chosen1edwin@gmail.com>
2026-01-03 14:01:12 +1100

    Fix vite base path for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/AWS_New_Batch_Trainee_Program/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/AWS_New_Batch_Trainee_Program/' : '/',
   plugins: [react()],
-})
+}))
